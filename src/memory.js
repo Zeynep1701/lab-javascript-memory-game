c5deae704ab3ef917cf8bda710dd20a3f9c1a7d8
Zeynep <zarazeynep5858@googlemail.com>
2023-09-13 17:43:00 +0200

    Solved lab

--- src/memory.js
+++ src/memory.js
@@ -2,17 +2,42 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
     // ... write your code here
-  }
+    if (!this.cards) {
+      return undefined
+    }
+
+    for (let i=0; i < this.cards.length -1; i++) {
+      let j = Math.floor(Math.random() * (this.cards.length -i) + i);
+      let temp = this.cards[i];
+      this.cards[i] = this.cards[j];
+      this.cards[j] = temp;
+    }
+    }
+  
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    let match = card1 === card2;
+    this.pairsClicked ++;
+    if (match) this.pairsGuessed ++;
+    this.pickedCards = [];
+    return match;
   }
 
   checkIfFinished() {
     // ... write your code here
+    if (this.pairsGuessed === this.cards.length/2) {
+      return true
+    }
+    else {
+      return false
+    }
   }
 }
